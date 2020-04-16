97e33351ab53b90b4ff380af36dd519b5729a915
Gaurav Gupta <37450514+ProgrammerGaurav@users.noreply.github.com>
2020-04-16 20:00:42 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system("clear")
-print("""\033[5;34;40m\n
+print("""
  ____  ____                 _   _   _             _
 |  _ \|  _ \  ___  ___     / \ | |_| |_ __ _  ___| | __
 | | | | | | |/ _ \/ __|   / _ \| __| __/ _` |/ __| |/ /
@@ -8,5 +8,6 @@ print("""\033[5;34;40m\n
 |____/|____/ \___/|___/ /_/   \_\__|\__\__,_|\___|_|\_\
 """)  
 os.system("chmod +x xerxes.c")
-a = input("\033[5;32;40m\n Enter website address eg : www.fakesite.com \n Website Name : ")
+os.system("gcc xerxes.c -o xerxes")
+a = input("\n Enter website address eg : www.fakesite.com \n Website Name : ")
 os.system("./xerxes "+a+" 80")
